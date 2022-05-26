5449975b5e17b6da9740a0b81604f50f3823167e
atom-marcel <65444694+atom-marcel@users.noreply.github.com>
2022-05-26 13:19:41 +0200

    Add files via upload

---
+++ point.py
@@ -0,0 +1,28 @@
+# @Author: Marcel Maluta
+# @Date:   2022-05-26T11:30:17+02:00
+# @Email:  marcelmaluta@gmail.com
+# @Last modified by:   Marcel Maluta
+# @Last modified time: 2022-05-26T12:54:04+02:00
+
+import pygame
+import math
+from vector import Vector
+import random
+
+class Point(object):
+    def __init__(self, x, y, origin_x, origin_y):
+        self.position = Vector(x, y)
+        self.origin = Vector(origin_x, origin_y)
+
+    def polar_move(self, r, phi):
+        x = int(r * math.cos(phi))
+        y = int(r * math.sin(phi))
+        self.set_position(x, y)
+
+    def set_position(self, x, y):
+        self.position.x = x
+        self.position.y = y
+        self.position.add_vector(self.origin)
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)), (self.position.x, self.position.y), 5)
---
+++ sim.py
@@ -0,0 +1,48 @@
+# @Author: Marcel Maluta
+# @Date:   2022-05-26T11:26:45+02:00
+# @Email:  marcelmaluta@gmail.com
+# @Last modified by:   Marcel Maluta
+# @Last modified time: 2022-05-26T13:14:54+02:00
+
+
+
+import pygame
+import math
+from point import Point
+import random
+
+window_x = 1920
+window_y = 1080
+
+sim_window = pygame.display.set_mode((window_x, window_y))
+clock = pygame.time.Clock()
+
+p = Point((window_x / 2) + 5, (window_y / 2) + 0, window_x / 2, window_y / 2)
+p2 = Point(p.position.x + 5, p.position.y + 0, p.position.x, p.position.y)
+
+j = 1
+r = 5
+r_inc = 2
+j_inc = 1
+
+resolution = 32
+phi = math.pi / resolution
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+
+    clock.tick(20)
+    phi = (j * math.pi) / resolution
+    p.polar_move(r, phi)
+    p.draw(sim_window)
+    j += j_inc
+    r += r_inc
+    pygame.display.flip()
+    if(r > 500):
+        r = 5
+        j = -1
+        j_inc *= -1
+        sim_window.fill((0, 0, 0))
